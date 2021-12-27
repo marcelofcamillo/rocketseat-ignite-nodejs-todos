cb520c3f721af336a9303fa50eb351ec4492f22f
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-12-27 11:11:54 -0300

    POST /users

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
+
+const users = [];
+
+// should be able to create a new user
+app.post('/users', (req, res) => {
+  const { name, username } = req.body;
+
+  const userExists = users.find((user) => user.username === username);
+
+  // should not be able to create a new user when username already exists
+  if (userExists) {
+    return res.status(400).json({ error: 'User already exists!' });
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+
+  users.push(user);
+
+  res.status(201).json(user);
+});
 
 app.listen(3000, () => {
   console.log('API started!');
